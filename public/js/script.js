81b47cc53f73156f6eb78313f2a019dc6ed84baf
FiernazIngga <inggapratama86@gmail.com>
2025-03-15 16:36:18 +0700

    htlku

---
+++ public/js/script.js
@@ -0,0 +1,14 @@
+const hamburger = document.querySelector('.hamburger-nav')
+const hatas = document.querySelector('.hatas')
+const htengah = document.querySelector('.htengah')
+const hbawah = document.querySelector('.hbawah')
+const menuDalam = document.querySelector('.menuDalam-nav')
+let gerak = 0;
+
+hamburger.addEventListener("click", () => {
+    gerak = !gerak;
+    gerak ? hatas.style.transform = "rotate(35deg)" : hatas.style.transform = "rotate(0deg)";
+    gerak ? htengah.style.opacity = "0" : htengah.style.opacity = "1";
+    gerak ? hbawah.style.transform = "rotate(-35deg)" : hbawah.style.transform = "rotate(0deg)";
+    gerak ? menuDalam.style.height = "10em" : menuDalam.style.height = "0px";
+})
